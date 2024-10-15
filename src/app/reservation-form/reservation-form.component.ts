ca1542e3ca670271c1b632a4acb848f9b5189c4b
Anngie Murillo <anngie.mur@gmail.com>
2024-10-14 23:58:17 -0300

    CRUD operations for reservation module

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService) {
+
+  }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      // required = if we don't create this value in the form, then it's not valid - every control inside a form has to be valid
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.reservationForm.valid){
+
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation)
+    }
+  }
 
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  // property
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    
+  }
+
+  // CRUD
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  // undefined bc there may not be a reservation for the id we input (return an undefined value = value doesn't exist)
+  getReservation(id: string): Reservation | undefined {
+    // take each reservation, and search for the one with the id that matches the param ID
+    return this.reservations.find(res => res.id === id);
+  }
+
+  // returns nothing
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
 }
